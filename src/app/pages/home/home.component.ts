302de5aa188c10d0954988a890712cd6d5e05594
YaelDonat <71334737+YaelDonat@users.noreply.github.com>
2021-12-17 10:42:37 +0100

    HomePage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,12 +4,18 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
+
 })
+
+
 export class HomeComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
+
 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
+const { blue } = require("tailwindcss/colors");
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      mars: colors.rose,
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      red: colors.red,
+      blue: colors.blue,
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
